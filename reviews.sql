417197a513066464725b4c19770f80b0e8550ce9
Nathan Fredericks <nathan@fredericks.dev>
2024-03-12 16:43:26 -0300

    Add reviews database initialization file

---
+++ reviews.sql
@@ -0,0 +1,20 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS reviews_db;
+
+-- Switch to the created database
+USE reviews_db;
+
+-- Create the reviews table
+CREATE TABLE IF NOT EXISTS review (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    review VARCHAR(250) NOT NULL,
+    added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE USER IF NOT EXISTS 'reviews_user'@'localhost' IDENTIFIED BY 'm0v13s';
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'reviews_user'@'localhost';
+
+-- Display the table structure
+DESCRIBE review;
