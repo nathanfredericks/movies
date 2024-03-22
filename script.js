f4ab4778e405d35a55b44a3e8c7bd99a44ea4e12
Nathan Fredericks <nathan@fredericks.dev>
2024-03-22 17:02:21 -0300

    Make search results responsive

--- script.js
+++ script.js
@@ -10,22 +10,25 @@ function search(){
     $.get("https://api.themoviedb.org/3/search/movie", {query: movie})
     .done(function(movies) {
         $("#results").empty()
-        if(movies.length == 0){
-            $("#results").append("No search results")
+        if (movies.total_results == 0) {
+            $("#results").append("<p>No search results.</p>");
+            return;
         }
-        console.log(movies)
         var finalresults = "";
-        finalresults += `<div class="row">`
+        finalresults += `<div class="row row-cols-2 row-cols-sm-3 row-cols-md-4">`
         movies.results.forEach(function(element){
             counter++;
             if(element.poster_path){
-                finalresults += `<div class="col"> <a href="/movies/movie.php?id=${element.id}"> <img src="https://image.tmdb.org/t/p/original/${element.poster_path}" height="300" width="200" alt="${element.title}"/><br>${element.title}<br> </a> </div>`
+                finalresults += `
+                    <div class="col text-center">
+                        <a href="/movies/movie.php?id=${element.id}">
+                            <img src="https://image.tmdb.org/t/p/w185/${element.poster_path}" alt="${element.title} poster"/>
+                        </a>
+                        <h1 class="fs-5 fw-medium">${element.title}</h1>
+                    </div>`
             }
             else{
                 finalresults += `<div class="col"> <a href="/movies/movie.php?id=${element.id}"> <img src="PlaceholderMovieImg.jpg" alt="${element.title}"> <br> ${element.title} <br> </a> </div>`
-            }
-            if(counter % 4 == 0 && counter != 0){
-                finalresults += `</div> <div class="row">`
             }
         })
         finalresults += "</div>"
