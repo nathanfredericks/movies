daba79dbb11afbbfc064d365243c1aecfb8a56b4
geoff <gn@bellaliant.net>
2024-03-22 14:25:18 -0300

    added hyperlinks to the results of the search bar

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function search(){
         console.log(movies)
         movies.results.forEach(function(element){
             if(element.poster_path){
-            $("#results").append(`<img src="https://image.tmdb.org/t/p/original/${element.poster_path}" width="200" alt="${element.title}"/><br>${element.title}<br>`);
+            $("#results").append(`<a href="http://localhost/movies/movie.php?id=${element.id}"> <img src="https://image.tmdb.org/t/p/original/${element.poster_path}" width="200" alt="${element.title}"/><br>${element.title}<br></a>`);
             }
             else{
                 $("#results").append(`no image <br> ${element.title} <br>`)
